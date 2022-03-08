93cc2deb497b0a7af9c81c2e983e80319b6ac06e
Ana Liebert <analiebert19@gmail.com>
2022-03-08 18:21:28 -0500

    initial testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+const todoItems = [
+  {
+    id: 1,
+    title: "Nature walk in the park",
+    description: "Visit the park with my friends",
+    completed: true
+  },
+
+  {
+    id: 2,
+    title: "Visit",
+    description: "Got to my aunt's place",
+    completed: true
+  },
+
+  {
+    id: 3,
+    title: "Write",
+    description: "Do an article about anthropology",
+    completed: true
+  },
+];
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <ul>
+//         {this.state.todoItems.map(item => (
+//           <div>
+//             <h1>{item.title}</h1>
+//             <span>{item.description}</span>
+//           </div>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {todoItems};
+  };
+  
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {this.state.todoItems.map(item => (
+          <div>
+            <h1>{item.title}</h1>
+            <span>{item.description}</span>
+          </div>
+        ))}
+      </ul>
     </div>
   );
+  }
 }
 
 export default App;
