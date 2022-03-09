91e30e8b173d5fa65beaefda73ea4cdd8739c6bd
Ana Liebert <analiebert19@gmail.com>
2022-03-09 14:54:17 -0500

    host detail page rendering

---
+++ src/pages/HostDetail.js
@@ -0,0 +1,43 @@
+import React from "react"
+import {useParams} from "react-router-dom"
+import { useState, useEffect } from "react"
+
+function HostDetail(props) {
+    const [hosts, setHosts] = useState(null);
+
+    const URL = "http://localhost:8000/mixtape/";
+    let foundHost
+    const hostId = useParams()
+    const foundHostId = Number(hostId.id)
+    console.log(foundHostId)
+
+    const getHosts = async () => {
+        const response = await fetch(URL + "hosts");
+        const data = await response.json();
+        console.log(data)
+        
+        foundHost = data.find(host => host.id === foundHostId)
+        setHosts(foundHost);
+        console.log("this is the found host", foundHost)
+    };
+
+    useEffect(() => getHosts(), []);
+
+    const loaded = () => {
+        return (
+            <div>
+                <img src={hosts.image}/>
+                <h1>{hosts.name}</h1>
+                <h1>{hosts.bio}</h1>
+            </div>
+        )
+    }
+    const loading = () => {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
+    return hosts ? loaded() : loading()
+}
+
+export default HostDetail
