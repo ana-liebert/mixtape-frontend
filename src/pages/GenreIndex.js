a0c529e1635c1541fc5c2b6d6e29dd4e22959895
Ana Liebert <analiebert19@gmail.com>
2022-03-25 12:30:30 -0400

    genre index styled

--- src/pages/GenreIndex.js
+++ src/pages/GenreIndex.js
@@ -4,12 +4,12 @@ import { useState, useEffect } from "react"
 import { Link } from "react-router-dom";
 
 function GenreIndex(props) {
-    
+
     const [mixes, setMixes] = useState(null);
 
     const URL = "https://aliebert-mixtape.herokuapp.com/mixtape/mixes/";
     // const URL = "http://localhost:8000/mixtape/mixtape/mixes";
-    
+
     let foundMixes
     const genreId = useParams()
     const foundGenreId = Number(genreId.id)
@@ -33,14 +33,33 @@ function GenreIndex(props) {
 
 
     const loaded = () => {
-        return mixes.map((mix) => (
-            <div className="card host-card" key={mix.id}>
-                <img src={mix.image} alt={mix.title} />
-                <Link to={`/mixes/${mix.id}`}><h1>{mix.title}</h1></Link>
-                <h3>{mix.description}</h3>
-                <iframe className="mix-test" width="100%" height="20" scrolling="no" title="mixtape" frameBorder="no" src={mix.soundcloudplayer}></iframe>
-            </div>
-        ));
+
+        // console.log(mixes.length)
+
+        if (mixes.length < 1) {
+            return (
+                <h1 className="mix-test">No mixes tagged with this genre yet. Check out some others!</h1>
+            )
+        }
+
+
+        return (
+        <div className="container genre-container">
+        <div className="row">
+            {mixes.map((mix) => {
+                return (
+                    <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 mycol">
+                        <div className="card host-card" key={mix.id}>
+                            <Link to={`/mixes/${mix.id}`}><h1>{mix.title}</h1></Link>
+                            <p>{mix.description}</p>
+                            <img class="host-img" src={mix.image} alt={mix.title} />
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
+        )
     };
 
     const loading = () => {
