4301be83cd095aafc47eca3209de6e83664d9703
Ana Liebert <analiebert19@gmail.com>
2022-03-31 11:00:32 -0400

    changes to create page host and creator id

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react"
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/esm/Button';
+import { useNavigate } from "react-router-dom";
 
 function Create(props) {
 
 
     const [mixes, setMixes] = useState(null);
 
+    const navigate = useNavigate()
+
 
     const URL = "https://aliebert-mixtape.herokuapp.com/mixtape/mixes/create/";
-    // const URL = "http://localhost:8000/mixtape/mixes/create";
+
 
 
     const getMixes = async () => {
@@ -19,18 +22,18 @@ function Create(props) {
     };
 
     console.log("genre props to map through", props.genres)
-    
+
     // state to hold formData
     const [newForm, setNewForm] = useState({
-            title: "",
-            description: "",
-            host: "",
-            genre: [4],
-            image: "",
-            soundcloudplayer: "",
-            creator: "",
-            tracklist: "",
-            
+        title: "",
+        description: "",
+        host: "",
+        genre: [4],
+        image: "",
+        soundcloudplayer: "",
+        creator: "",
+        tracklist: "",
+
     })
     console.log(newForm)
 
@@ -58,213 +61,136 @@ function Create(props) {
     //     {label: "Synth Pop", value: "2"},
     // ]
 
-// const options = props.genres.map(
-//     (genre) => <option key={genre.id} value={genre.id}>
-//         {genre.name} </option>
-//         )
+    // const options = props.genres.map(
+    //     (genre) => <option key={genre.id} value={genre.id}>
+    //         {genre.name} </option>
+    //         )
 
-// const handleGenreChange = event => {
-//     console.log("this is the genre change", event.target)
-//     setGenreInput({[event.target.name]: event.target.value })
-// }
-    
-    
+    // const handleGenreChange = event => {
+    //     console.log("this is the genre change", event.target)
+    //     setGenreInput({[event.target.name]: event.target.value })
+    // }
+
+
     const handleSubmit = event => {
         event.preventDefault()
-        console.log(newForm.genre)
+        // console.log(newForm.genre)
         props.createMix(newForm)
-        // props.createGenre(genreInput)
-        // how can i get specific genre change in new form? 
-        setNewForm({
-            title: "",
-            description: "",
-            host: "",
-            genre: [4],
-            image: "",
-            soundcloudplayer: "",
-            creator: "",
-            tracklist: "",
-        })
+        props.createGenre(genreInput)
+        // setNewForm({
+        //     title: "",
+        //     description: "",
+        //     host: "",
+        //     genre: [4],
+        //     image: "",
+        //     soundcloudplayer: "",
+        //     creator: "",
+        //     tracklist: "",
+        // })
+        navigate("/")
     }
 
     const loaded = () => {
         return (
-        <section>
+            <section>
 
 
-           <Form className="update"> 
-            <h1>Update</h1>
-        <Form.Group className="mb-3" >
-            <Form.Label>Title</Form.Label>
-            <Form.Control 
-             type="text"
-             value={newForm.title}
-             name="title"
-             placeholder={mixes.title}
-             onChange={handleChange}
-            />
-        </Form.Group>
+                <Form className="update">
+                    <h1>New mix</h1>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={newForm.title}
+                            name="title"
+                            placeholder={mixes.title}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
 
-        <Form.Group className="mb-3" >
-            <Form.Label>Description</Form.Label>
-            <Form.Control
-             type="text"
-             value={newForm.description}
-             name="description"
-             placeholder={mixes.description}
-             onChange={handleChange}
-            />
-        </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={newForm.description}
+                            name="description"
+                            placeholder={mixes.description}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
 
-        <Form.Group className="mb-3" >
-            <Form.Label>Host</Form.Label>
-            <Form.Control
-             type="text"
-             value={newForm.host}
-             name="host"
-             placeholder={mixes.host}
-             onChange={handleChange}
-            />
-        </Form.Group>
+                    <Form.Group className="mb-3" >
+                        {/* <Form.Label>Host</Form.Label> */}
+                        <Form.Control
+                            type="hidden"
+                            value="6"
+                            name="host"
+                            placeholder={mixes.host}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
 
-        <Form.Group className="mb-3" >
-            <Form.Label>Image</Form.Label>
-            <Form.Control
-             type="text"
-             value={newForm.image}
-             name="image"
-             placeholder={mixes.image}
-             onChange={handleChange}
-            />
-        </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Image</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={newForm.image}
+                            name="image"
+                            placeholder={mixes.image}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
 
-        <Form.Group className="mb-3" >
-            <Form.Label>soundcloudplayer</Form.Label>
-            <Form.Control
-             type="text"
-             value={newForm.soundcloudplayer}
-             name="soundcloudplayer"
-             placeholder={mixes.soundcloudplayer}
-             onChange={handleChange}
-            />
-        </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>soundcloudplayer</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={newForm.soundcloudplayer}
+                            name="soundcloudplayer"
+                            placeholder={mixes.soundcloudplayer}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
 
-        <Form.Group className="mb-3" >
-            <Form.Label>Creator</Form.Label>
-            <Form.Control
-             type="text"
-             value={newForm.creator}
-             name="creator"
-             placeholder={mixes.creator}
-             onChange={handleChange}
-            />
-        </Form.Group>
+                    <Form.Group className="mb-3" >
+                        {/* <Form.Label>Creator</Form.Label> */}
+                        <Form.Control
+                            type="hidden"
+                            value="1"
+                            name="creator"
+                            placeholder={mixes.creator}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
 
-        <select multiple onChange={(event) => setGenreInput(event.target.value)} value={genreInput}>
-                    
-                    {props.genres.map(
-                        (genre) => <option key={genre.id} value={genre.id}>
-                            {genre.name}
-                        </option>
-                    )}
-                </select>
-                
-                <br>
-                </br>
+                    <select multiple onChange={(event) => setGenreInput(event.target.value)} value={genreInput}>
 
-        <Button variant="primary" type="submit" onClick={handleSubmit}>
-            Submit
-        </Button>
-        
-        </Form>
+                        {props.genres.map(
+                            (genre) => <option key={genre.id} value={genre.id}>
+                                {genre.name}
+                            </option>
+                        )}
+                    </select>
 
+                    <br>
+                    </br>
 
+                    <Button variant="primary" type="submit" onClick={handleSubmit}>
+                        Submit
+                    </Button>
 
+                </Form>
 
-
-            {/* <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={newForm.title}
-                    name="title"
-                    placeholder="title"
-                    onChange={handleChange}
-                />
-                <input
-                    type="text"
-                    value={newForm.description}
-                    name="description"
-                    placeholder="description"
-                    onChange={handleChange}
-                />
-                <input
-                    type="text"
-                    value={newForm.host}
-                    name="host"
-                    placeholder="host"
-                    onChange={handleChange}
-                />
-
-                <label>
-                Select genre tags
-                </label>
-                this only works when page is already loaded and then you add it in??
-                
-                <select onChange={(event) => setGenreInput(event.target.value)} value={genreInput}>
-            
-                    {props.genres.map(
-                        (genre) => <option key={genre.id} value={genre.id}>
-                            {genre.name}
-                        </option>
-                    )}
-                
-                </select>
-
-            
-
-                <input
-                    type="text"
-                    value={newForm.image}
-                    name="image"
-                    placeholder="image"
-                    onChange={handleChange}
-                />
-                <input
-                    type="text"
-                    value={newForm.soundcloudplayer}
-                    name="soundcloudplayer"
-                    placeholder="soundcloudplayer"
-                    onChange={handleChange}
-                />
-                <input
-                    type="text"
-                    value={newForm.creator}
-                    name="creator"
-                    placeholder="creator"
-                    onChange={handleChange}
-                /> 
-                <input
-                    type="textarea"
-                    value={newForm.tracklist}
-                    name="tracklist"
-                    placeholder="tracklist"
-                    onChange={handleChange}
-                />
-
-
-
-                <input type="submit" value="Create Mix" />
-            </form> */}
             </section>
-    )
-}
+        )
+    }
 
-const loading = () => {
+    const loading = () => {
 
-    return (
-        <h1>Loading...</h1>
-    )
-}
+        return (
+            <h1>Loading...</h1>
+        )
+    }
 
     return mixes ? loaded() : loading()
 
