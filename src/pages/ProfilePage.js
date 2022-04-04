1d96d3bade4e4168810eb12f4c09a5f35b7f06b3
Ana Liebert <analiebert19@gmail.com>
2022-04-04 19:15:09 -0400

    useeffect removed from profpage

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -143,7 +143,7 @@
 
 
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
